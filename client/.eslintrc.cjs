b194ac13607f4d8742db5ddf268abae8f061e9eb
mojsun <m.khorashahi7@gmail.com>
2024-07-25 21:06:10 -0400

    another try4

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -24,8 +24,8 @@ module.exports = {
   ],
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
   settings: { react: { version: "18.2" } },
-  plugins: ["react-refresh"],
+  plugins: [],
   rules: {
-    "react-refresh/only-export-components": "warn",
+    // "react-refresh/only-export-components": "warn",
   },
 };
